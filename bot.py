3645c9f70d071272ae18ddac2d503ee99e1f837f
Doria <143899929+ssnti1@users.noreply.github.com>
2025-09-01 23:53:45 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -142,7 +142,7 @@ async def auto_post():
         except Exception as e:
             print("❌ error en auto_post:", e)
 
-        await asyncio.sleep(10)
+        await asyncio.sleep(40)
 
 # ================== MAIN ==================
 async def main():
